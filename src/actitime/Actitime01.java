5c19c06cb0daed295ec642eaeeb3baffad266271
admin <admin@DESKTOP-4KM33U9>
2019-11-21 09:38:54 +0530

    Actitime project

---
+++ src/actitime/Actitime01.java
@@ -0,0 +1,18 @@
+package actitime;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Actitime01 {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	
+@Test
+void login(){
+	WebDriver driver=new ChromeDriver();
+	
+	
+	}
+}
